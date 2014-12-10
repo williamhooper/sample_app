7e3e071c0b419badbd4fec7157206b65eab18fae
Edward Hooper <william.hooper@reachlocal.com>
2014-12-09 21:32:28 -0800

    Finish user signup; fix previous errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validate :password, presence: true, length: { minimum: 6 }
-  validate :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
